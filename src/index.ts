b27098893da58005f8c90179b2e1397b0a8991bb
R-unic <roleyspersonal@gmail.com>
2024-12-01 22:47:53 -0500

    fix: error if you destroy twice

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export default class Destroyable {
 	protected readonly janitor = new Janitor;
 
 	public destroy(): void {
+		if (!("Destroy" in this.janitor)) return;
 		this.janitor.Destroy();
 	}
 }
